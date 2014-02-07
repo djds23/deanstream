62dbc191e41757c6b5bb1ed0b447280f391d86e0
Dean <dean.silfen@gmail.com>
2014-02-07 13:22:59 -0500

    tried to fix javascript again, failed again

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,5 @@
 $('a#new_video').bind('click', function() {
-  var a_button = $("a#new_video");
-  var video_source = $("video").attr("src");
-  var video = $("video");
+  var video = $("video")
   $.getJSON($SCRIPT_ROOT + '/_new_video',function(data) {
     $video.removeAttr("src")
     $video.attr("src",data.stream)
