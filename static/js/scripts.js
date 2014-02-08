0e70755819032e8167b1008e08f2ea9f9b689e4c
Dean <dean.silfen@gmail.com>
2014-02-08 15:35:34 -0500

    got the jquery almost working.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,9 @@ $(function() {
   var video = function(e) {
     $.getJSON($SCRIPT_ROOT +'/_new_video', {
     }, function(data) {
-      $('video').removeAttr
-    }
+      $('video').removeAttr('src');
+      $('video').Attr('src', data.stream);
+    });
   }
-}
+  $("a#new_video").bind('click', video);
+});
