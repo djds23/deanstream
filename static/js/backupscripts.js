d708625c1774c22c4db22f91ade5c5282788ac3c
Dean <dean.silfen@gmail.com>
2014-02-15 15:43:15 -0500

    CHANGED REQUIREMENTS.TXT, attempts at implementing db

--- static/js/backupscripts.js
+++
@@ -1,7 +0,0 @@
-$('a#new_video').bind('click', function() {
-  var video = $("video")
-  $.getJSON($SCRIPT_ROOT + '/_new_video',function(data) {
-    $video.removeAttr("src");
-    $video.attr("src",data.stream);
-});
-});
