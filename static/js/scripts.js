b6f0550a1a248ca90590d7c81dd786acedd9ddc2
Dean <dean.silfen@gmail.com>
2014-02-22 15:11:34 -0500

    fix js and templates, the random sql element picker is still broken

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,7 +7,7 @@ $(function() {
         var webm_path = $SCRIPT_ROOT+"static/"+data.webm;
         var mp4_path = $SCRIPT_ROOT+"static/"+data.mp4;
         var poster_path = $SCRIPT_ROOT+"static/img/mobile.jpg";
-        $(".insert-video").append('<video autoplay="" loop="" poster="' + poster_path +'">\
+        $(".insert-video").html('<video autoplay="" loop="" poster="' + poster_path +'">\
                                   <source type="video/webm" src="'+ webm_path +'">\
                                   <source type="video/mp4" src="'+ mp4_path +'">\
                                   </video></br>');
