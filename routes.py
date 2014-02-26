d2c7bccf8a607f53f63e1828622b7370e9b21d2c
Dean <dean.silfen@gmail.com>
2014-02-25 20:24:45 -0500

    remove useless template

--- routes.py
+++ routes.py
@@ -10,10 +10,7 @@ config = app.config.from_object('config')
 @app.route('/')
 def home():
     '''creates the dict with the filepaths, and returns the template'''
-    agent = request.headers.get('User-Agent')
-    webm = 'webm/couch.webm'
-    mp4 = 'mp4/couch.mp4'
-    return render_template('video.html', webm=webm, mp4=mp4)
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
@@ -25,16 +22,14 @@ def contact():
 
 @app.route('/_new_video')
 def new_video():
-    current_mp4 = request.args.get('current_mp4')
-    current_webm = request.args.get('current_webm')
+    current_id = request.args.get('current_id')
     while True:
         video_id = random.randrange(1,models.Video.query.count() + 1) # adds one to the limit so that it raises above 1
-        new_video = models.Video.query.get(video_id)
-        test_case = str(new_video)
-        if current_mp4.split('/')[-1] in test_case and current_webm.split('/')[-1]:
+        if video_id != current_id:
+            new_video = models.Video.query.get(video_id)
             webm = new_video.get_webm()
             mp4 = new_video.get_mp4()
-            return jsonify(webm=webm,mp4=mp4)
+            return jsonify(webm=webm,mp4=mp4,video_id=video_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,17 +3,18 @@ $(function() {
     $.getJSON(
       $SCRIPT_ROOT +'/_new_video',
       {
-        current_webm: $('source#webm').attr('src'),
-        current_mp4:  $('source#mp4').attr('src')
+        current_id:   $('span#id_number').val()
       },
       function(data) {
+        var id_number = data.id;
         var webm_path = $SCRIPT_ROOT+"static/"+data.webm;
         var mp4_path = $SCRIPT_ROOT+"static/"+data.mp4;
         var poster_path = $SCRIPT_ROOT+"static/img/mobile.jpg";
         $(".insert-video").html('<video autoplay="" loop="" poster="' + poster_path +'">\
                                   <source id="webm" type="video/webm" src="'+ webm_path +'">\
                                   <source id="mp4" type="video/mp4" src="'+ mp4_path +'">\
-                                  </video></br>');
+                                  <span id="id_number"></span>\
+                                </video></br>');
       });
   }
   $("a#new_video").bind('click', video);
