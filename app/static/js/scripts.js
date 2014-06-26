127c147f6a5df1647b56dfdbd9404d40ccafd384
Dean Silfen <dean.silfen@gmail.com>
2014-06-26 15:15:49 -0400

    added flask script and some js

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -13,7 +13,7 @@ $(function() {
         var webm_path = $SCRIPT_ROOT+"static/"+data.webm;
         var mp4_path = $SCRIPT_ROOT+"static/"+data.mp4;
         var poster_path = $SCRIPT_ROOT+"static/img/mobile.jpg";
-        $(".insert-video").html('<video autoplay="" loop="">\
+        $(".insert-video").html('<video class="video-js" autoplay="" loop="">\
                                   <source id="webm" type="video/webm" src="'+ webm_path +'">\
                                   <source id="mp4" type="video/mp4" src="'+ mp4_path +'">\
                                   <img src="'+poster_path +'">\
